66479cad936d2d6dd03f264f7e47ee35d6fd1300
Riddhim Jain <riddhimsandeepjain@gmail.com>
2026-03-03 22:56:29 +0530

    Fix missing Toast import in BlockerService.kt

--- app/src/main/java/com/example/appblocker/BlockerService.kt
+++ app/src/main/java/com/example/appblocker/BlockerService.kt
@@ -6,6 +6,7 @@ import android.os.Handler
 import android.os.Looper
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
+import android.widget.Toast
 
 class BlockerService : AccessibilityService() {
 
